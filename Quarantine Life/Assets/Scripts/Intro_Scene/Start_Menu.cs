d9cb38f2c268091d2d6f523ecd03e0d25fafda8f
Unknown <nas244@msstate.edu>
2020-05-03 00:14:29 -0500

    Beginnings

---
+++ Quarantine Life/Assets/Scripts/Intro_Scene/Start_Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Start_Menu : MonoBehaviour
+{
+    [SerializeField]
+    private LevelLoader Loader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void OnStartButton()
+    {
+        Loader.LoadLevel("");
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
